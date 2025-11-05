06ec736b362795d5dbc0f5448864c2f67808a001
vincent-philippe <vincent.philippe18@gmail.com>
2025-11-05 10:15:16 -0500

    small fixes

--- setup_csv_holehe.sh
+++ setup_csv_holehe.sh
@@ -9,7 +9,7 @@ echo "INSTALLING HOLEHE IN /etc/megadose/holehe"
 sudo git clone https://github.com/megadose/holehe.git /etc/megadose/holehe
 
 # tmp fix issue https://github.com/megadose/holehe/issues/209 (not merged atm)
-sed -i '/WORKDIR/a RUN pip install --upgrade pip setuptools wheel requests && pip install .' Dockerfile
+sudo sed -i '/WORKDIR/a RUN pip install --upgrade pip setuptools wheel requests && pip install .' /etc/megadose/holehe/Dockerfile
 
 sudo docker build /etc/megadose/holehe/. -t my-holehe-image
 
