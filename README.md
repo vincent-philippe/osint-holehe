a38a1842edc19d211209817803c6698f191bae83
vincent-philippe <vincent.philippe18@gmail.com>
2025-11-05 10:13:16 -0500

    fix broken docker 'request' objet missing megadose/holehe#209'

---
+++ README.md
@@ -0,0 +1,5 @@
+# Usage
+
+Install dependencies through running `sh setup_csv_holehe.sh` 
+
+Run holehe analyzis from you csv file such as `sh holehe.sh list.csv` 
--- setup_csv_holehe.sh
+++ setup_csv_holehe.sh
@@ -7,3 +7,11 @@ sudo apt update \
 echo "INSTALLING HOLEHE IN /etc/megadose/holehe"
 
 sudo git clone https://github.com/megadose/holehe.git /etc/megadose/holehe
+
+# tmp fix issue https://github.com/megadose/holehe/issues/209 (not merged atm)
+sed -i '/WORKDIR/a RUN pip install --upgrade pip setuptools wheel requests && pip install .' Dockerfile
+
+sudo docker build /etc/megadose/holehe/. -t my-holehe-image
+
+sudo rm -rf /etc/megadose
+
